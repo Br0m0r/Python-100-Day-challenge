9854fdd32ff905cd245389cf47f2b4483186e572
Bromor <158762685+Br0m0r@users.noreply.github.com>
2024-02-19 12:01:40 +0200

    Started

---
+++ PythonApplication1 - Day 1/PythonApplication1 - Day 1/PythonApplication1___Day_1.py
@@ -0,0 +1,59 @@
+'''
+
+print ("Hello")
+print("and how are you")
+
+
+print("""               
+THIS        IS
+
+
+
+THE USE OF 
+
+TRIPLE QUOTES            
+""")
+
+
+
+
+print("Bromor")
+print("19/2/2024")
+print("""I ll be commiting some time every day for 100 days in learning Python !""")
+
+
+input("whats your name?:") # this wont store the input i give
+
+myName = input("Whats your name? : ")
+myAge = input("How old are you? : ")
+print("Thats not too old!")
+VScode =input("Do you like VSCode?")
+print("OF COURSE YOU DO")
+
+print("So lets summarize :")    
+print(myName)
+print(myAge) 
+             #variables cant have spaces
+             
+'''
+
+print("Getting to know you")
+name = input("Whats your name : ")
+age = input("How old are you : ")
+food = input("Whats your favourite food")
+game = input("Whats your favourite game")
+
+print("So your name is : ")
+print(name)
+print("You are : ")
+print(age) 
+print(" years old")
+print("you like eating :  ")
+print(food)
+print("You play a lot of :  ")
+print(game)
+
+
+
+
+
