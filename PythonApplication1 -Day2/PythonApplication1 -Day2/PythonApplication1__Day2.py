80ffc1e35e420086c8551024d88be6c6cfc3eb56
Bromor <158762685+Br0m0r@users.noreply.github.com>
2024-02-20 20:20:48 +0200

    add

---
+++ PythonApplication1 -Day2/PythonApplication1 -Day2/PythonApplication1__Day2.py
@@ -0,0 +1,27 @@
+'''
+myname = input("Whats your name,mate? :");
+myLunch = input("What are you having for lunch: ");
+print(myname,"will be feasting on some great",myLunch,"later on!");
+
+
+Food = input("Name a type of food ");
+Plant = input("Name a type of plant ");
+Method = input("Name a method of cooking ");
+Ruined = input("Describe burned food ");
+
+print("The Menu today! : ",Food,Method,"with",Plant,"and",Ruined ,"for some flavour,enjoy!" );
+'''
+
+
+
+name = input("What is your name? ")
+superpower = input("What is your superpower? ")
+weakness = input("What is your weakness? ")
+rival_name = input("What is your rival's name? ")
+
+
+print("this is the story of ",name,"who had the power of",superpower,)
+print("One day he went to pick berries in the forest and he stumbled upon",rival_name,"his arch nemesis");
+print("together they fought fiercely,despite",rival_name,"using",name,"'s",weakness,"to his advantage");
+print("in the end",name,"emerged as the victor,the end!!HURRAY!!")
+    
