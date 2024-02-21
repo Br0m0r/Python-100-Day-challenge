7e9a37937667437869813a0a6cdeea3441df145a
Bromor <158762685+Br0m0r@users.noreply.github.com>
2024-02-21 20:11:18 +0200

    new day - new entry

---
+++ PythonApplication1 Day - 3/PythonApplication1 Day - 3/PythonApplication1_Day___3.py
@@ -0,0 +1,45 @@
+'''myName = input("What is your name ?");
+if myName == "memos" :
+    print("Welcome back mate!");
+    print("You are learning python");
+else:
+    print("Hello,nice to meet you!") #should always be indendt;
+    
+input("Guess the character!");
+input("Type 'Y' for yes or 'N' for no");
+patrick = input("is mayonaise an instrument??");
+if patrick == "Y":
+    print("Hello Patrick!");
+else:
+    print("Then you aint Patrick!");
+
+spongebob = input("Do you like bubbles?");
+if spongebob == "Y":
+    print("Hi spongebob!");
+else:
+    print("Then you aint Spongebob!");
+    
+
+
+
+print("SECURE LOGIN")
+username = input("Username > ")
+password = input("password > ")
+
+if username == "mark" and password == "password" :
+    print("welcome mark");
+elif username =="Suzan" and password =="Suzan12345" :
+    print("Hi Suzan");
+else:
+    print("Who are you!");
+    '''
+   
+print("LOGIN");
+username = input("Insert Username ");
+password = input("Insert Password ");
+if username == "bob" and password == "12345" :
+      print("Oi bob,long time no see!");
+elif username == "pete" and password == "123456":
+      print("Welcome back boss");
+else:
+      print("Speak friend and enter,but you cant either way so dont bother XD");      
