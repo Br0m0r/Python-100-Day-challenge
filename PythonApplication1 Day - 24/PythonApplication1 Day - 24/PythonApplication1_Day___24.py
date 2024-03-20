5b84ab8666e4b04d16bfeb245b62c39f5ad47861
Bromor <158762685+Br0m0r@users.noreply.github.com>
2024-03-20 11:18:07 +0200

    new

---
+++ PythonApplication1 Day - 24/PythonApplication1 Day - 24/PythonApplication1_Day___24.py
@@ -0,0 +1,66 @@
+# Todo List Manager
+# should add,remove,view and edit todoList
+
+
+# import time,os
+# from colorama import init, Fore, Back, Style
+# init() 
+
+# myList = []
+
+# def printList():
+#     print()
+#     for i in range(len(myList)):
+#         print(f'{i}: {myList[i]}')
+#     print() 
+    
+# def editList():
+#     printList()  
+#     itemI = input("Select item number to edit: ")
+#     ToAddNew = input(f"Enter new value for item {itemI}: ")
+#     myList[int(itemI)] = ToAddNew
+#     time.sleep(1)
+#     os.system("cls")
+
+# while True:
+#     print("-----------------")
+#     print("ToDo List Manager")
+#     print("-----------------")
+#     print()
+#     print("1.Add\n2.Remove\n3.View\n4.Edit")
+#     print("")
+#     act = input("What would you like to do? : ")
+#     if act == "1":
+#         add = input("Add item to List: ")
+#         myList.append(add)
+#         time.sleep(0.5)
+#         os.system("cls")
+#     elif act == "2":
+#         remove = input("Remove item from List: ")
+#         if remove in myList:
+#             myList.remove(remove)
+#         else:
+#             print("There is no such item in List")
+#     elif act == "3":
+#         time.sleep(0.5)
+#         os.system("cls")
+#         printList()
+#         time.sleep(2)
+#         os.system("cls")
+#     elif act == "4":
+#         time.sleep(0.5)
+#         os.system("cls")
+#         editList()
+#         printList()
+#         time.sleep(2)
+#         os.system("cls")
+
+
+
+        
+        
+        
+        
+            
+    
+
